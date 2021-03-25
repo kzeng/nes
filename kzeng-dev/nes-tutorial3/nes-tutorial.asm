5508c3de503cdc44fc6e9b2711d034f5fbf13aa7
kaizeng <kai.zeng@neophotonics.com>
2021-03-25 09:07:36 +0800

    add some codes

---
+++ kzeng-dev/nes-tutorial3/nes-tutorial.asm
@@ -0,0 +1,117 @@
+; NES Game Development Tutorial
+;
+; Author: Jonathan Moody
+; Github: https://github.com/jonmoody
+
+  .inesprg 1    ; Defines the number of 16kb PRG banks
+  .ineschr 1    ; Defines the number of 8kb CHR banks
+  .inesmap 0    ; Defines the NES mapper
+  .inesmir 1    ; Defines VRAM mirroring of banks
+
+  .rsset $0000
+pointerBackgroundLowByte  .rs 1
+pointerBackgroundHighByte .rs 1
+
+  .bank 0
+  .org $C000
+
+RESET:
+  JSR LoadBackground
+  JSR LoadPalettes
+  JSR LoadAttributes
+
+  LDA #%10000000   ; Enable NMI, sprites and background on table 0
+  STA $2000
+  LDA #%00011110   ; Enable sprites, enable backgrounds
+  STA $2001
+  LDA #$00         ; No background scrolling
+  STA $2006
+  STA $2006
+  STA $2005
+  STA $2005
+
+InfiniteLoop:
+  JMP InfiniteLoop
+
+LoadBackground:
+  LDA $2002
+  LDA #$20
+  STA $2006
+  LDA #$00
+  STA $2006
+
+  LDA #LOW(background)
+  STA pointerBackgroundLowByte
+  LDA #HIGH(background)
+  STA pointerBackgroundHighByte
+
+  LDX #$00
+  LDY #$00
+.Loop:
+  LDA [pointerBackgroundLowByte], y
+  STA $2007
+
+  INY
+  CPY #$00
+  BNE .Loop
+
+  INC pointerBackgroundHighByte
+  INX
+  CPX #$04
+  BNE .Loop
+  RTS
+
+LoadPalettes:
+  LDA $2002
+  LDA #$3F
+  STA $2006
+  LDA #$00
+  STA $2006
+
+  LDX #$00
+.Loop:
+  LDA palettes, x
+  STA $2007
+  INX
+  CPX #$20
+  BNE .Loop
+  RTS
+
+LoadAttributes:
+  LDA $2002
+  LDA #$23
+  STA $2006
+  LDA #$C0
+  STA $2006
+  LDX #$00
+.Loop:
+  LDA attributes, x
+  STA $2007
+  INX
+  CPX #$40
+  BNE .Loop
+  RTS
+
+NMI:
+  RTI
+
+  .bank 1
+  .org $E000
+
+background:
+  .include "graphics/background.asm"
+
+palettes:
+  .include "graphics/palettes.asm"
+
+attributes:
+  .include "graphics/attributes.asm"
+
+  .org $FFFA
+  .dw NMI
+  .dw RESET
+  .dw 0
+
+  .bank 2
+  .org $0000
+  .incbin "graphics.chr"
